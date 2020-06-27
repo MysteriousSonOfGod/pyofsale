12b7f019fa9a8bc52cfba9211264a6cefbba0e2c
nicmorais <nmorais.st@gmail.com>
2020-06-27 14:53:43 -0300

    Moves ui stuff to separated file

--- pyofsale.py
+++ pyofsale.py
@@ -3,12 +3,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 
 from PyQt5.QtWidgets import *
-from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel, QSqlQueryModel
 
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel, QSqlQueryModel
 from modules.addOrEditDialog import addOrEdit
+
 from modules.settswin import Ui_settingsWindow
 from modules.viewSale import Ui_viewSale
 from modules.newSale import Ui_newSaleWin
+
 import ast
 import json
 
@@ -16,9 +18,11 @@ import json
 with open('settings.json') as jsonconfigs:
     configsfile = json.load(jsonconfigs)
 
+
 class Ui_mainwindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(Ui_mainwindow, self).__init__(parent=parent)
+
         self.setupUi(self)
 
     def setupUi(self, mainwindow):
@@ -37,6 +41,7 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
         self.tabWidget.setGeometry(QtCore.QRect(20, 10, 750, 530))
         self.tabWidget.setObjectName("tabWidget")
+        self.tabWidget.tabBarClicked.connect(self.customerSearch)
 
         self.productsTab = QtWidgets.QWidget()
         self.productsTab.setObjectName("productsTab")
@@ -49,11 +54,6 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.tableView.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
         self.tableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.tableView.doubleClicked['QModelIndex'].connect(self.onclick)
-
-        if configsfile["showVerticalHeader"]:
-            self.tableView.verticalHeader().setVisible(True)
-        else:
-            self.tableView.verticalHeader().setVisible(False)
 
         self.lineEdit = QtWidgets.QLineEdit(self.productsTab)
         self.lineEdit.setGeometry(QtCore.QRect(20, 30, 610, 25))
@@ -87,7 +87,6 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
 
         self.calendarWidget = QtWidgets.QCalendarWidget(self.sellTab)
         self.calendarWidget.setGeometry(QtCore.QRect(20, 10, 310, 171))
-        self.calendarWidget.setMinimumDate(QtCore.QDate(2010, 9, 14))
         self.calendarWidget.setGridVisible(True)
         self.calendarWidget.setHorizontalHeaderFormat(QtWidgets.QCalendarWidget.ShortDayNames)
         self.calendarWidget.setVerticalHeaderFormat(QtWidgets.QCalendarWidget.NoVerticalHeader)
@@ -108,6 +107,13 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
 
         self.salesTableView.doubleClicked['QModelIndex'].connect(self.showSale)
 
+        self.salesComboBox = QtWidgets.QComboBox(self.sellTab)
+        self.salesComboBox.setGeometry(QtCore.QRect(610, 30, 121, 25))
+        self.salesComboBox.setObjectName("salesComboBox")
+        self.salesComboBox.addItem("All")
+        self.salesComboBox.addItem("Finished Only")
+        self.salesComboBox.currentIndexChanged.connect(self.searchDate)
+
         if configsfile["showVerticalHeader"]:
             self.salesTableView.verticalHeader().setVisible(True)
         else:
@@ -121,6 +127,66 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.viewBtn.setGeometry(QtCore.QRect(650, 300, 80, 71))
         self.viewBtn.setObjectName("viewBtn")
         self.viewBtn.clicked.connect(self.showSale)
+
+        self.customersTab = QtWidgets.QWidget()
+        self.customersTab.setEnabled(True)
+        self.customersTab.setAccessibleName("")
+        self.customersTab.setObjectName("customersTab")
+
+        self.customersTableView = QtWidgets.QTableView(self.customersTab)
+        self.customersTableView.setGeometry(QtCore.QRect(20, 70, 611, 391))
+        self.customersTableView.setObjectName("customersTableView")
+        self.customersTableView.horizontalHeader().setStretchLastSection(True)
+        self.customersTableView.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.customersTableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+
+        if configsfile["showVerticalHeader"]:
+            self.tableView.verticalHeader().setVisible(True)
+            self.customersTableView.verticalHeader().setVisible(True)
+        else:
+            self.tableView.verticalHeader().setVisible(False)
+            self.customersTableView.verticalHeader().setVisible(False)
+
+        self.searchCustomer = QtWidgets.QLineEdit(self.customersTab)
+        self.searchCustomer.setGeometry(QtCore.QRect(20, 30, 611, 25))
+        self.searchCustomer.setObjectName("searchCustomer")
+        self.searchCustomer.returnPressed.connect(self.customerSearch)
+        self.searchCustomer.textChanged.connect(self.customerSearch)
+
+        self.searchCustomerMode = QtWidgets.QComboBox(self.customersTab)
+        self.searchCustomerMode.setGeometry(QtCore.QRect(640, 30, 91, 25))
+        self.searchCustomerMode.setObjectName("comboBox")
+        self.searchCustomerMode.addItem("Name")
+        self.searchCustomerMode.addItem("Phone")
+        self.searchCustomerMode.addItem("Address")
+        self.searchCustomerMode.addItem("E-mail")
+        self.searchCustomerMode.currentIndexChanged.connect(self.customerSearch)
+
+        self.startsOrContains = QtWidgets.QComboBox(self.customersTab)
+        self.startsOrContains.setGeometry(QtCore.QRect(640, 70, 91, 25))
+        self.startsOrContains.setObjectName("comboBox_2")
+        self.startsOrContains.addItem("Starts with")
+        self.startsOrContains.addItem("Contains")
+        self.startsOrContains.currentIndexChanged.connect(self.customerSearch)
+
+        self.clearBtn = QtWidgets.QPushButton(self.customersTab)
+        self.clearBtn.setGeometry(QtCore.QRect(608, 30, 21, 25))
+        self.clearBtn.setText("")
+        icon = QtGui.QIcon.fromTheme("edit-clear")
+        self.clearBtn.setIcon(icon)
+        self.clearBtn.setFlat(True)
+        self.clearBtn.setObjectName("clearBtn")
+        self.clearBtn.clicked.connect(self.searchCustomer.clear)
+
+        self.addCustomerBtn = QtWidgets.QPushButton(self.customersTab)
+        self.addCustomerBtn.setGeometry(QtCore.QRect(650, 390, 80, 71))
+        self.addCustomerBtn.setObjectName("addCustomerBtn")
+
+        self.editCustomerBtn = QtWidgets.QPushButton(self.customersTab)
+        self.editCustomerBtn.setGeometry(QtCore.QRect(650, 300, 80, 71))
+        self.editCustomerBtn.setObjectName("editCustomerBtn")
+
+        self.tabWidget.addTab(self.customersTab, "Customers")
 
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
@@ -141,17 +207,19 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.actionSettings.setObjectName("actionSettings")
         self.actionAbout = QtWidgets.QAction(mainwindow)
         self.actionAbout.setObjectName("actionAbout")
-        self.actionSair = QtWidgets.QAction(mainwindow)
-        self.actionSair.setObjectName("actionSair")
+        self.actionExit = QtWidgets.QAction(mainwindow)
+        self.actionExit.setObjectName("actionExit")
+
+        self.getMaxMinDate()
 
         self.menuFile.addAction(self.actionSettings)
-        self.menuFile.addAction(self.actionSair)
+        self.menuFile.addAction(self.actionExit)
         self.aboutMenu.addAction(self.actionAbout)
-        self.aboutMenu.addAction(self.actionSair)
+        self.aboutMenu.addAction(self.actionExit)
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.aboutMenu.menuAction())
 
-        self.actionSair.triggered.connect(self.closeFunction)
+        self.actionExit.triggered.connect(self.closeFunction)
         self.actionSettings.triggered.connect(self.openconfigs)
         self.actionAbout.triggered.connect(self.aboutFunction)
 
@@ -168,14 +236,34 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.db = QSqlDatabase.addDatabase("QSQLITE")
         self.db.setDatabaseName("pyofsaledb.db")
         self.db.open()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS sales (SALEID INTEGER PRIMARY KEY AUTOINCREMENT, SALEPRODS "
-                         "TEXT, SALETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%S', 'NOW','LOCALTIME')),SALETOTAL TEXT, "
-                         "FINISHED BOOLEAN DEFAULT '0');")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS sales (SALEID INTEGER PRIMARY KEY AUTOINCREMENT, SALEPRODS TEXT, "
+                         "SALETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%S', 'NOW','LOCALTIME')),SALETOTAL TEXT,"
+                         "FINISHED BOOLEAN DEFAULT '0', CUSTOMID INTEGER, FOREIGN KEY (CUSTOMID) REFERENCES "
+                         "customers(CUSTOMID));")
         self.cur.execute("CREATE TABLE IF NOT EXISTS products (PRODID INTEGER PRIMARY KEY AUTOINCREMENT, BARCODE "
                          "TEXT, DESC TEXT NOT NULL UNIQUE, PRICE REAL DEFAULT '0', QTD INTEGER DEFAULT '0');")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS customers (CUSTOMID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT "
+                         "NOT NULL UNIQUE, EMAIL TEXT, PHONE TEXT, ADDRESS TEXT);")
+
+    def getMaxMinDate(self):
+        self.cur.execute("SELECT SALETIME FROM sales LIMIT 1;")
+        minDate = self.cur.fetchone()
+        if minDate is not None:
+            print("min", minDate)
+            minDate = ((minDate[0])[:10])
+            self.calendarWidget.setMinimumDate(QtCore.QDate(int(minDate[:4]), int(minDate[5:7]), int(minDate[8:])))
+
+            self.cur.execute("SELECT SALETIME FROM sales ORDER BY SALETIME DESC LIMIT 1;")
+            maxDate = self.cur.fetchone()
+            print("max", maxDate)
+            maxDate = ((maxDate[0])[:10])
+            self.calendarWidget.setMaximumDate(QtCore.QDate(int(maxDate[:4]), int(maxDate[5:7]), int(maxDate[8:])))
+        else:
+            self.calendarWidget.setEnabled(False)
+            self.salesComboBox.setEnabled(False)
+            self.viewBtn.setEnabled(False)
 
     def rowprint(self, primarikeycolumn, primaykey, table):
-
         self.cur.execute("SELECT * FROM " + table + " WHERE " + str(primarikeycolumn) + " =" + str(primaykey))
         row = self.cur.fetchone()
         return row
@@ -188,8 +276,12 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
 
     def searchDate(self):
         date = self.calendarWidget.selectedDate()
-        sqlstrdate = "SELECT SALEID, SALETIME, SALETOTAL, FINISHED FROM sales WHERE SALETIME LIKE '" + str(
+        sqlstrdate = "SELECT SALEID, SALETIME, SALETOTAL, FINISHED,NAME FROM sales,customers WHERE " \
+                     "sales.CUSTOMID=customers.CUSTOMID AND SALETIME LIKE '" + str(
             date.toPyDate()) + "%'"
+
+        if self.currentIndex() == 1:
+            sqlstrdate += " AND FINISHED = 1"
         self.queryModel = QSqlQueryModel()
 
         self.queryModel.setQuery(sqlstrdate)
@@ -266,6 +358,26 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.conn.commit()
         self.searchString()
 
+    def customerSearch(self):
+        if self.searchCustomerMode.currentIndex() == 0:
+            column = "NAME"
+        elif self.searchCustomerMode.currentIndex() == 1:
+            column = "PHONE"
+        elif self.searchCustomerMode.currentIndex() == 2:
+            column = "ADDRESS"
+        else:
+            column = "EMAIL"
+        wildcard1 = "'"
+        if self.startsOrContains.currentIndex() == 1:
+            wildcard1 = "'%"
+        wildcard2 = "%'"
+
+        sqlstr = "SELECT EMAIL,PHONE,ADDRESS,NAME FROM customers WHERE " + column + " LIKE " + wildcard1 + self.searchCustomer.text() + wildcard2
+
+        self.queryModel = QSqlQueryModel()
+        self.queryModel.setQuery(sqlstr)
+        self.customersTableView.setModel(self.queryModel)
+
     def retranslateUi(self, mainwindow):
         _translate = QtCore.QCoreApplication.translate
         self.addbutton.setText(_translate("mainwindow", "Add"))
@@ -277,8 +389,10 @@ class Ui_mainwindow(QtWidgets.QMainWindow):
         self.aboutMenu.setTitle(_translate("mainwindow", "About"))
         self.actionSettings.setText(_translate("mainwindow", "Settings"))
         self.actionAbout.setText(_translate("mainwindow", "About"))
-        self.actionSair.setText(_translate("mainwindow", "Exit"))
+        self.actionExit.setText(_translate("mainwindow", "Exit"))
         self.viewBtn.setText(_translate("MainWindow", "View"))
+        self.editCustomerBtn.setText(_translate("MainWindow", "Edit"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.customersTab), _translate("MainWindow", "Customers"))
 
 
 import sys
